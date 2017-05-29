a8924f95e0989a8c22025d3f247d68763745cb97
krisi0505 <krisi0505@abv.bg>
2017-05-29 11:19:52 +0300

    add temperature diagram

--- server/app/public/js/views/home.js
+++ server/app/public/js/views/home.js
@@ -39,14 +39,50 @@ $(document).ready(function(){
 	var dashboardButton = document.getElementById('dashboards-btn');
 	dashboardButton.addEventListener('click', showDashboards);
 
+	var dashboardsDiv = document.getElementById('dashboards');
+	dashboardsDiv.style.display = 'none';
+
 	function showDashboards(){
 		accountContainer.style.display = 'none';
-	   
+		dashboardsDiv.style.display = '';
+		var dataPoints = [{y : 10}, {y : 13}, {y : 18}, {y : 20}, {y : 17}];
+		var chart = new CanvasJS.Chart("dashboards", {
+				title : {
+					text : "Temperature"
+				},
+				data : [{
+						type : "spline",
+						dataPoints : dataPoints
+					}
+				]
+			});
+
+		chart.render();
+		
+		var yVal = 15, updateCount = 0;
+		chart.options.title.text = "Temperature";
+		var updateChart = function () {
+
+			yVal = yVal + Math.round(5 + Math.random() * (-5 - 5));
+	      	updateCount++;
+			
+			dataPoints.push({
+				y : yVal
+			});
+	        
+			chart.render();    
+			
+		};
+
+		// update chart every second
+		setInterval(function(){updateChart()}, 1000);
 	}
 
 	function showAccountSettings(){
-		// customize the account settings form 
+		dashboardsDiv.style.display = 'none';
 		accountContainer.style.display = '';
+
+		// customize the account settings form 
 		$('#account-form h2').text('Account Settings');
 		$('#account-form #sub1').text('Here are the current settings for your account.');
 		$('#user-tf').attr('disabled', 'disabled');
